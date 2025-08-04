df793619ae055a311e853c1f050ca66de026bab5
Brazil <stephanie_brazil@dell.com>
2025-08-04 11:01:18 -0300

    Fixing test class issue

--- src/test/java/com/example/demo1/repository/UserServiceTest.java
+++ src/test/java/com/example/demo1/repository/UserServiceTest.java
@@ -5,11 +5,12 @@ import com.example.demo1.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 public class UserServiceTest {
 
     @Autowired
