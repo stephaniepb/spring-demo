ec8c4fb23ad3ae8ee3e9c13592c3a4e13f8dcead
Brazil <stephanie_brazil@dell.com>
2025-08-05 09:09:30 -0300

    Creating new test classes

--- src/main/java/com/example/demo1/model/Product.java
+++ src/main/java/com/example/demo1/model/Product.java
@@ -1,11 +1,13 @@
 package com.example.demo1.model;
 
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@Builder
 @Document(collection = "product")
 @Data
 @NoArgsConstructor
--- src/test/java/com/example/demo1/service/ProductServiceTest.java
+++ src/test/java/com/example/demo1/service/ProductServiceTest.java
@@ -1,31 +1,29 @@
 package com.example.demo1.service;
 
-import com.example.demo1.model.User;
+import com.example.demo1.model.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class UserServiceTest {
+public class ProductServiceTest {
 
     @Autowired
-    private UserService userService;
+    private ProductService productService;
 
     @Test
-    public void saveUser() {
+    public void saveProductTest() {
 
-        User user = User.builder()
-                .name("Stephanie")
-                .email("stephanie@email.com")
+        Product product = Product.builder()
+                .name("Rice")
                 .build();
 
-        User save = userService.save(user);
+        Product save = productService.save(product);
 
         assertThat(save).isNotNull();
-        assertThat(save.getName()).isEqualTo("Stephanie");
+        assertThat(save.getName()).isEqualTo("Rice");
     }
 
 }
